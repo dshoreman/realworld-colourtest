0c26a0ce9347815ba9556fce91c147ccffb60682
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-03-22 14:53:05 +0000

    Add prompt variant with default colours

--- colours.sh
+++ colours.sh
@@ -8,6 +8,7 @@ grey=$'\e[38;5;236m'
 white=$'\e[37m'
 red=$'\e[31m'
 green=$'\e[32m'
+blue=$'\e[34m'
 cyan=$'\e[36m'
 yellow=$'\e[33m'
 magenta=$'\e[35m'
@@ -17,17 +18,34 @@ bgWhite='\e[48;5;7m'
 bgRed='\e[48;5;52m'
 bgGreen='\e[48;5;22m'
 bgBGreen='\e[48;5;2m'
+bgBlue='\e[48;5;4m'
 bgReset='\e[49m'
+
+zsh_prompt() {
+    local bg dir text sep=''
+
+    if [ "$1" = "green" ]; then
+        bg=${bgBGreen}
+        dir=${green}
+        text=${grey}
+    else
+        bg=${bgBlue}
+        dir=${blue}
+        text=${white}
+    fi
+
+    echo
+    echo -en "${bgWhite}${black} 16:20:42 ${bg}${white}${sep}${text} ~/dev/project ${bgWhite}${dir}${sep}"
+    echo -e "${black}  master ${bgReset}${white}${sep}${reset}"
+    echo -e "${bold}${green}\$${reset} ${2}"
+}
 
 echo -e "${dim}Dim line of text       ${cyan}Dim info message       ${yellow}Dim warning message       ${red}Dim error message${reset}"
 echo -e "Normal line of text    ${cyan}Normal info message    ${yellow}Normal warning message    ${red}Normal error message${reset}"
 echo -e "${bold}Bold line of text      ${cyan}Bold info message      ${yellow}Bold warning message      ${red}Bold error message${reset}"
-echo
 
-sep=''
-echo -en "${bgWhite}${black} 16:20:42 ${bgBGreen}${white}${sep}${grey} ~/dev/project ${bgWhite}${green}${sep}"
-echo -e "${black}  master ${bgReset}${white}${sep}${reset}"
-echo -e "${bold}${green}\$ ${reset}git --no-pager diff"
+zsh_prompt blue
+zsh_prompt green "git --no-pager diff"
 
 echo -e "${dim}${cyan}diff --git a/foo.sh b/foo.sh"
 echo -e "index abcdef0..93f3a4d 100755"
